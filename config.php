3ede8bf462a85ff06115cb7d230ccba3ee964625
olaulau <olaulau@gmail.com>
2014-04-25 00:28:14 +0200

    - usage of DOMDocument instead of SimpleXML (deletion ...) - filtering to output only transformed items

--- config.php
+++ config.php
@@ -4,6 +4,7 @@ $firstname = "Flo";
 
 $replacement_patterns = array(
 	"du stagiaire" => "de $firstname",
+	"je" => "$firstname",
 );
 
 ?>
--- rss.php
+++ rss.php
@@ -1,43 +1,78 @@
 <?php
 
-require_once 'mysql_config.php';
+// require_once 'mysql_config.php';
 require_once 'config.php';
 
 
 
 
 $rss_url = "http://lesjoiesducode.fr/rss";
-//TODO : download
-$rss = simplexml_load_file("rss"); // from dev cache
+//TODO : download ( wget http://lesjoiesducode.fr/rss )
 
+$documentString = file_get_contents("rss");
+$dom = new DOMDocument();
+$dom->loadXML($documentString);
 
-//echo "<pre>";
-//print_r($xml); die;
+// echo "<pre>";
+// print_r($rss); die;
 
-$item = $rss->channel->item;
-$nb_item = $item->count();
+$rss = $dom->childNodes->item(0);
+// print_r($rss); die;
+$channel = $rss->childNodes->item(0);
+// print_r($channel); die;
 
+$searches = array_keys($replacement_patterns);
+$replaces = array_values($replacement_patterns);
 
-foreach($rss->channel->item as $item) {
-	$title = $item->title;
-	foreach($replacement_patterns as $search => $replace) {
-		$title = str_replace($search, $replace, $title);
-	}
-	$item->title = $title;
-
-
-//	print_r($item);
-//	echo "-------------------------------------" . PHP_EOL;
+$items = array();
+foreach($channel->childNodes as $item)
+{
+	/* @var $item DOMNode */
+	// 	print_r($item);
+	if($item->nodeName=='item')
+	{
+		$items[] = $item; // stack items for further compute
+	}	
 }
 
-//echo "</pre>";
+foreach ($items as $item) { //compute the stack
+	/* @var $item DOMNode */
+// 	print_r($item);
+	foreach($item->childNodes as $itemChild)
+	{
+		if($itemChild->nodeName=='title')
+		{
+			/* @var $itemChild DOMNode */
+			// 	print_r($itemChild);
+			$title = $itemChild->nodeValue;
+			// 	var_dump($title);
+	
+			$title = str_replace($searches, $replaces, $title, $count);
+			// 	continue;
+			if($count > 0) {
+// 				echo $title . PHP_EOL;
+				$itemChild->nodeValue = $title;
+			}
+			else {
+				$channel->removeChild($item);
+			}
+		}
+			
+	}
+}
 
-$rss->channel->title = "Les joies du flug";
-$rss->channel->link = "http://www.lesjoiesduflug.fr.am/";
 
 
+
+// echo "</pre>";
+
+
+
+//TODO : recode with DOMDocument API
+// $rss->channel->title = "Les joies du flug";
+// $rss->channel->link = "http://www.lesjoiesduflug.fr.am/";
 
 header('Content-Type:text/xml; charset=utf-8');
-echo $rss->asXML();
+echo $dom->saveXML();
 
 ?>
