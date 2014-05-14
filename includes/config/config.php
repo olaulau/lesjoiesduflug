9a0304872b631d65f8a0bf7c825d44477f3a0004
olaulau <olaulau@gmail.com>
2014-05-14 23:51:12 +0200

    add name parameter to customize name replacement via URL : can host multiple flugs easily

--- includes/config/config.php
+++ includes/config/config.php
@@ -4,11 +4,14 @@ $rss_url = "http://lesjoiesducode.fr/rss";
 $rss_cache_file = "cache/rss";
 $rss_cache_lifetime = 5*60; // 5 min
 
-$firstname = "Flo";
+$name = "Flo";
+if(isset($_REQUEST['name']) && !empty($_REQUEST['name'])) {
+	$name = $_REQUEST['name'];
+}
 
 $replacement_patterns = array(
-	"du stagiaire" => "de $firstname",
-	"je" => "$firstname",
+	"du stagiaire" => "de $name",
+	"je" => "$name",
 );
 
 ?>
